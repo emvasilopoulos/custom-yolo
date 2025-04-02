4ebf5803de8bf9e9e2a62bed770aef08171534f1
Emmanouil Vasilopoulos <emmanouilvasilopoulos@gmail.com>
2025-04-02 14:09:07 +0300

    dataset.coco.tasks.instances: implement abstract methods & refactor

--- src/custom_yolo_lib/dataset/coco/tasks/instances.py
+++ src/custom_yolo_lib/dataset/coco/tasks/instances.py
@@ -1,10 +1,12 @@
 import pathlib
-from typing import Any, Dict, List, Tuple
+from typing import List, Tuple
 
-import torch
+import pandas as pd
 
+import custom_yolo_lib.image_size
 import custom_yolo_lib.io.read
 import custom_yolo_lib.dataset.coco.tasks.base
+import custom_yolo_lib.dataset.object
 import custom_yolo_lib.process.bbox
 
 ANNOTATIONS_GROUPED_KEY = "annotations_grouped_by_image_id"
@@ -18,21 +20,52 @@ class COCOInstances2017(
     which groups annotations by image_id.
     """
 
+    def _extract_image_path(self, sample: pd.DataFrame) -> pathlib.Path:
+        image_filename = self._image_file_name_from_id(sample["image_id"].values[0])
+        return self.images_dir / image_filename
+
+    def _extract_objects(
+        self, sample: pd.DataFrame, image_size: custom_yolo_lib.image_size.ImageSize
+    ):
+        x1s = sample["x1"].values
+        y1s = sample["y1"].values
+        ws = sample["w"].values
+        hs = sample["h"].values
+        class_ids = sample["category_id"].values
+        objects_ = []
+        for x1, y1, w, h, class_id in zip(x1s, y1s, ws, hs, class_ids):
+            bbox = custom_yolo_lib.process.bbox.Bbox(
+                x1=x1 / image_size.width,
+                y1=y1 / image_size.height,
+                w=w / image_size.width,
+                h=h / image_size.height,
+                is_normalized=True,
+            )
+            objects_.append(
+                custom_yolo_lib.dataset.object.Object(
+                    bbox=bbox,
+                    class_id=class_id,
+                )
+            )
+        return objects_
+
     def _get_coco_item(
         self, idx: int
-    ) -> Tuple[torch.Tensor, List[custom_yolo_lib.process.bbox.Bbox]]:
-        self.annotations = self.annotations[self.image_ids[idx]]
-        raise NotImplementedError()
+    ) -> Tuple[pathlib.Path, List[custom_yolo_lib.dataset.object.Object]]:
+        groups = self.original_groups_indexed[idx]
+        sample = self.annotations.get_group(groups)
+
+        image_path = self._extract_image_path(sample)
+        image_size = custom_yolo_lib.io.read.read_image_dimensions(image_path)
+        objects_ = self._extract_objects(sample, image_size)
+
+        return image_path, objects_
 
     def _define_length(self):
-        return len(self.image_ids)
-
-    def get_pair(self, idx: int) -> Tuple[torch.Tensor, Dict[str, Any]]:
-        raise NotImplementedError()
+        return len(self.original_groups_indexed)
 
     def _read_annotations(self, annotations_path: pathlib.Path):
-        all_annotations = custom_yolo_lib.io.read.read_csv(annotations_path).groupby(
+        self.annotations = custom_yolo_lib.io.read.read_csv(annotations_path).groupby(
             "image_id"
         )
-        self.annotations = all_annotations
         self.original_groups_indexed = list(self.annotations.groups.keys())
