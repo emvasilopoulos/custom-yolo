4b3f85a74f29d722d26636667a0ba1269af6b20e
Manos <emmanouilvasilopoulos@gmail.com>
2025-04-13 21:11:13 +0300

    model.heads.test_detections: use new parameter

--- test/model/heads/test_detections.py
+++ test/model/heads/test_detections.py
@@ -18,7 +18,7 @@ class TestDetectionHead(unittest.TestCase):
         input_tensor = torch.randn(
             (1, 1024, 13, 13)
         )  # Batch size of 1, 1024 channels, 13x13 feature map
-        output_tensor = model(input_tensor)
+        output_tensor = model(input_tensor, training=False)
         self.assertEqual(output_tensor.anchor1_output.shape, (1, 85, 13, 13))
 
     def test_forward2(self):
@@ -27,7 +27,7 @@ class TestDetectionHead(unittest.TestCase):
         )
         input_tensor = torch.randn((1, 1024, 20, 20))
         # Batch size of 1, 1024 channels, 20x20 feature map
-        output_tensor = model(input_tensor)
+        output_tensor = model(input_tensor, training=False)
         self.assertEqual(output_tensor.anchor1_output.shape, (1, 85, 20, 20))
 
     def test_forward_all_anchors_shapes(self):
@@ -37,7 +37,7 @@ class TestDetectionHead(unittest.TestCase):
         input_tensor = torch.randn((1, 512, 26, 26))
 
         # Batch size of 1, 512 channels, 26x26 feature map
-        anchor_outs = model(input_tensor)
+        anchor_outs = model(input_tensor, training=False)
 
         out1, out2, out3 = anchor_outs
         self.assertEqual(out1.shape, (1, 85, 26, 26))
@@ -54,7 +54,7 @@ class TestDetectionHead(unittest.TestCase):
         input_tensor = torch.zeros((1, 1, 13, 13))
         input_tensor[0, 0, :6, :] = 1.0
 
-        out = model(input_tensor)
+        out = model(input_tensor, training=False)
         out1, out2, out3 = out.anchor1_output, out.anchor2_output, out.anchor3_output
         for i in range(13):
             # x
@@ -76,7 +76,7 @@ class TestDetectionHead(unittest.TestCase):
 
         input_tensor = torch.zeros((1, 1, 13, 13))
         input_tensor[0, 0, :6, :] = 1.0
-        out1, out2, out3 = model(input_tensor)
+        out1, out2, out3 = model(input_tensor, training=False)
 
         # h
         self.assertTrue((-b <= out1[0, 2]).all())
