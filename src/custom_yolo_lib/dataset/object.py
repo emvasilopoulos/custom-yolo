a5f1ee1faccd2cf198a94307c57f0a19f0996e5b
Manos <emmanouilvasilopoulos@gmail.com>
2025-04-17 09:36:28 +0300

    dataset.object: add 'to_tensor' functionality

--- src/custom_yolo_lib/dataset/object.py
+++ src/custom_yolo_lib/dataset/object.py
@@ -1,5 +1,7 @@
 import dataclasses
 
+import torch
+
 import custom_yolo_lib.process.bbox
 
 
@@ -7,3 +9,8 @@ import custom_yolo_lib.process.bbox
 class Object:
     bbox: custom_yolo_lib.process.bbox.Bbox
     class_id: int
+
+    def to_tensor(self) -> torch.Tensor:
+        return torch.tensor(
+            [self.bbox.x, self.bbox.y, self.bbox.w, self.bbox.h, self.class_id]
+        )
