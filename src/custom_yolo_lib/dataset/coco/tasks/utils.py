e5b070b21d6ac6c5ac4e4b429d6424fad2eb3c76
Manos <emmanouilvasilopoulos@gmail.com>
2025-04-19 18:15:01 +0300

    dataset.coco.tasks: separate function to avoid circular imports (part 2 of commit 0847b74667297ad3f79f1d5ec2c22b7c998d0a49)

--- src/custom_yolo_lib/dataset/coco/tasks/utils.py
+++ src/custom_yolo_lib/dataset/coco/tasks/utils.py
@@ -1,12 +1,8 @@
 import enum
-import pathlib
 
-import pandas as pd
 import torch
 
-import custom_yolo_lib.io.read
 import custom_yolo_lib.dataset.object
-import custom_yolo_lib.dataset.coco.tasks.instances
 
 
 class AnnotationsType(enum.Enum):
@@ -103,37 +99,3 @@ def object_to_tensor(
     x[4] = 1.0  # objectness
     x[5 + object_.class_id] = 1.0  # class_id
     return x
-
-
-def convert_grouped_instances_json_to_csv(json_path: pathlib.Path):
-    annotations = custom_yolo_lib.io.read.read_json(json_path)[
-        custom_yolo_lib.dataset.coco.tasks.instances.ANNOTATIONS_GROUPED_KEY
-    ]
-    annotations_for_df = {
-        "image_id": [],
-        "category_id": [],
-        "x1": [],
-        "y1": [],
-        "w": [],
-        "h": [],
-        "area": [],
-        "iscrowd": [],
-        "id": [],
-        "segmentation": [],
-    }
-
-    for item in annotations.keys():
-        for annotation in annotations[item]:
-            annotations_for_df["image_id"].append(item)
-            annotations_for_df["category_id"].append(annotation["category_id"])
-            annotations_for_df["x1"].append(annotation["bbox"][0])
-            annotations_for_df["y1"].append(annotation["bbox"][1])
-            annotations_for_df["w"].append(annotation["bbox"][2])
-            annotations_for_df["h"].append(annotation["bbox"][3])
-            annotations_for_df["area"].append(annotation["area"])
-            annotations_for_df["iscrowd"].append(annotation["iscrowd"])
-            annotations_for_df["id"].append(annotation["id"])
-            annotations_for_df["segmentation"].append(annotation.get("segmentation"))
-
-    df = pd.DataFrame(annotations_for_df)
-    df.to_csv(json_path.with_suffix(".csv"), index=False)
