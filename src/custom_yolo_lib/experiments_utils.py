fb3be422ad806ea31983f61cc3e766adb6dbea5a
Manos Vasilopoulos <manos@seervision.com>
2025-04-23 08:08:52 +0000

    custom_yolo_lib: add experiments_utils

---
+++ src/custom_yolo_lib/experiments_utils.py
@@ -0,0 +1,29 @@
+import pathlib
+
+
+def make_experiment_dir(
+    experiment_name: str,
+    base_dir: pathlib.Path,
+) -> pathlib.Path:
+    """
+    Create a directory for the experiment with the given name and base directory.
+    The directory will be created if it does not exist.
+
+    Args:
+        experiment_name (str): Name of the experiment.
+        base_dir (pathlib.Path): Base directory where the experiment directory will be created.
+
+    Returns:
+        pathlib.Path: Path to the experiment directory.
+    """
+    experiment_dir = base_dir / experiment_name
+    if not experiment_dir.exists():
+        experiment_dir.mkdir(parents=True, exist_ok=True)
+    else:
+        # Create a new directory with a counter if the directory already exists
+        counter = 1
+        while (experiment_dir / f"{experiment_name}_{counter}").exists():
+            counter += 1
+        experiment_dir = experiment_dir / f"{experiment_name}_{counter}"
+        experiment_dir.mkdir(parents=True, exist_ok=False)
+    return experiment_dir
