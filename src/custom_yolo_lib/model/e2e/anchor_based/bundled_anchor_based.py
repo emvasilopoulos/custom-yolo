a80670a65e7e81e880d27405c7e913eda8cb79a7
Manos Vasilopoulos <manos@seervision.com>
2025-04-28 20:37:43 +0000

    custom_yolo_lib.model.e2e.anchor_based: update

--- src/custom_yolo_lib/model/e2e/anchor_based/bundled_anchor_based.py
+++ src/custom_yolo_lib/model/e2e/anchor_based/bundled_anchor_based.py
@@ -1,4 +1,3 @@
-import math
 from typing import List, Tuple
 import torch
 
@@ -30,7 +29,41 @@ class YOLOModel(torch.nn.Module):
             1024, num_classes, feature_map_type=FeatureMapType.LARGE
         )
 
-    def forward(self, x: torch.Tensor) -> List[DetectionHeadOutput]:
+        self.anchors_per_head = self.detect_small.num_anchors
+        self.feats_per_head = self.detect_small.feats_per_anchor
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        small, medium, large = self.backbone(x)
+        small, medium, large = self.neck(small, medium, large)
+        out_small = self.detect_small(small, self.__training)
+        out_medium = self.detect_medium(medium, self.__training)
+        out_large = self.detect_large(large, self.__training)
+        return torch.cat(
+            [
+                out_small.view(
+                    out_small.shape[0], self.anchors_per_head, self.feats_per_head, -1
+                ),
+                out_medium.view(
+                    out_medium.shape[0], self.anchors_per_head, self.feats_per_head, -1
+                ),
+                out_large.view(
+                    out_large.shape[0], self.anchors_per_head, self.feats_per_head, -1
+                ),
+            ],
+            dim=3,
+        )  # (batch_size, 3, 85, grid_x1*grid_y1 + grid_x2*grid_y2 + grid_x3*grid_y3)
+
+    def train_forward(self, x: torch.Tensor) -> List[DetectionHeadOutput]:
+        small, medium, large = self.backbone(x)
+        small, medium, large = self.neck(small, medium, large)
+        out_small = self.detect_small.train_forward(small, self.__training)
+        out_medium = self.detect_medium.train_forward(medium, self.__training)
+        out_large = self.detect_large.train_forward(large, self.__training)
+        return out_small, out_medium, out_large
+
+    def train_forward2(
+        self, x: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         small, medium, large = self.backbone(x)
         small, medium, large = self.neck(small, medium, large)
         out_small = self.detect_small(small, self.__training)
